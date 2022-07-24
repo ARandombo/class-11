d14bb95692025e6ec242552a7547f62cae51a509
ARandombo <109843073+ARandombo@users.noreply.github.com>
2022-07-24 14:01:15 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+//Dont need to declare everything.
+var trex ,trex_running;
+function preload(){
+ trex_running=loadAnimation("trex1.png","trex3.png","trex4.png") 
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  
+  //create a trex sprite
+ trex = createSprite (50,160,20,50)
+ trex.addAnimation("running",trex_running)
+ trex.scale=0.5
+ ground =createSprite (200,180,550,10)
+}
+
+function draw(){
+  background("white")
+  if (keyDown("space"))
+ {
+  trex.velocityY=-10
+ } 
+ //give velocity lesser than 1
+trex.velocityY = trex.velocityY +0.8
+trex.collide(ground)
+
+drawSprites()
+}
